158894ab8dec5e802115cf17aab423f144d402ca
Newiihuhu <Ninewii_huhu@hotmail.com>
2021-05-21 00:38:56 +0700

    add savepage

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@ include('connectDB.php');
 include('header.php');
 session_start();
 $mem_username = $_SESSION['mem_username'];
-$sql = "select mem_fname,mem_lname,mem_email
+$sql = "select mem_username,mem_password,mem_fname,mem_lname,mem_email,mem_tel
         from member where mem_username = '" . $mem_username . "'";
 $query = mysqli_query($cid, $sql);
 $result = mysqli_fetch_array($query);
@@ -21,8 +21,26 @@ $result = mysqli_fetch_array($query);
             <div>
                 <div>
                     <form>
+                        <label>Username </label>
+                        <input type="text" id="mem_username" name="mem_username" style="margin-top: 50px;margin-left: 35px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_username'] ?>" readonly>
+                    </form>
+                </div>
+
+            </div>
+            <div>
+                <div>
+                    <form>
+                        <label>Password </label>
+                        <input type="text" id="mem_password" name="mem_password" style="margin-left: 35px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_password'] ?>">
+                    </form>
+                </div>
+
+            </div>
+            <div>
+                <div>
+                    <form>
                         <label>Firstname </label>
-                        <input type="text" id="Name" style="margin-top: 50px;margin-left: 35px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_fname'] ?>">
+                        <input type="text" id="mem_fname" name="mem_fname" style=" margin-left: 35px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_fname'] ?>">
                     </form>
                 </div>
 
@@ -31,7 +49,7 @@ $result = mysqli_fetch_array($query);
                 <div>
                     <form>
                         <label>Lastname</label>
-                        <input type="text" id="Username" style="margin-left: 7px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_lname'] ?>">
+                        <input type="text" id="mem_lname" name="mem_lname" style="margin-left: 7px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_lname'] ?>">
                     </form>
                 </div>
 
@@ -40,7 +58,7 @@ $result = mysqli_fetch_array($query);
                 <div>
                     <form>
                         <label>Email</label>
-                        <input type="text" id="Email" style="margin-left: 40px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_email'] ?>">
+                        <input type="text" id="mem_email" name="mem_email" style="margin-left: 40px;border-radius: 25px;text-align: center;" value="<?php echo $result['mem_email'] ?>">
                     </form>
                 </div>
 
@@ -49,7 +67,7 @@ $result = mysqli_fetch_array($query);
                 <div>
                     <form>
                         <label>Tel. </label>
-                        <input type="text" id="Password" style="margin-left: 13px;border-radius: 25px;text-align: center;" value="<?php echo $_SESSION["mem_tel"]; ?>">
+                        <input type="text" id="mem_tel" name="mem_tel" style="margin-left: 13px;border-radius: 25px;text-align: center;" value="<?php echo $result["mem_tel"]; ?>">
                     </form>
                 </div>
 
@@ -65,35 +83,33 @@ $result = mysqli_fetch_array($query);
         </div>
 
     </div>
-
+    <script type="text/javascript">
+        function save0Click() {
+            $.ajax({
+                url: "saveinfoProfile.php",
+                type: "POST",
+                data: {
+                    mem_password: $("#mem_password").val(),
+                    mem_fname: $("#mem_fname").val(),
+                    mem_lname: $("#mem_lname").val(),
+                    mem_email: $("#mem_email").val(),
+                    mem_tel: $("#mem_tel").val()
+                },
+                beforeSend: function() {
+                    $('#loadMe').modal('show');
+                },
+                complete: function() {
+                    $('#loadMe').modal('hide');
+                },
+                success: function(data) {
+                    alert('save success');
+                    window.location = "homepage.php";
+                }
+            })
+        }
+    </script>
 </body>
-<script>
-    
-    function save0Click() {
-        $.ajax({
-            url: "saveinfoProfile.php",
-            type: "POST",
-            data: {
-                mem_username: $("#mem_username").val(),
-                mem_password: $("#mem_password").val(),
-                mem_fname: $("#mem_fname").val(),
-                mem_lname: $("#mem_lname").val(),
-                mem_email: $("#mem_email").val(),
-                mem_tel: $("#mem_tel").val()
-            },
-            beforeSend: function() {
-                $('#loadMe').modal('show');
-            },
-            complete: function() {
-                $('#loadMe').modal('hide');
-            },
-            success: function(data) {
-                alert('save success');
-                window.location = "homepage.php";
-            }
-        })
-    }
-</script>
+
 
 
 </html>
--- saveinfoProfile.php
+++ saveinfoProfile.php
@@ -2,14 +2,16 @@
 session_start();
 include('connectDB.php');
 $mem_username = $_SESSION['mem_username'];
-$mem_password = $_POST['mem_password']; 
+$mem_password = $_POST['mem_password'];
 $mem_fname = $_POST['mem_fname'];
 $mem_lname = $_POST['mem_lname'];
 $mem_email = $_POST['mem_email'];
 $mem_tel = $_POST['mem_tel'];
 
+
 $sql = "update member set
-mem_fname = '".$mem_fname."'
+mem_password = '".$mem_password."',
+mem_fname = '".$mem_fname."',
 mem_lname = '".$mem_lname."',
 mem_email = '".$mem_email."',
 mem_tel = '".$mem_tel."'
